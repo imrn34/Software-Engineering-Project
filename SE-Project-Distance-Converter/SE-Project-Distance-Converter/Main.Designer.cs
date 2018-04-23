c724372844443bd8311e367639e1c7c2a3e9f45c
imrn34 <38562645+imrn34@users.noreply.github.com>
2018-04-23 18:37:18 +0300

    Design
    1 Form
    2 ComboBox
    2 TextBox
    4 Label
    1 Button

---
+++ SE-Project-Distance-Converter/SE-Project-Distance-Converter/Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SE_Project_Distance_Converter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lefttext = new System.Windows.Forms.TextBox();
+            this.righttext = new System.Windows.Forms.TextBox();
+            this.leftcom = new System.Windows.Forms.ComboBox();
+            this.rightcom = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lefttext
+            // 
+            this.lefttext.Location = new System.Drawing.Point(149, 90);
+            this.lefttext.Name = "lefttext";
+            this.lefttext.Size = new System.Drawing.Size(120, 20);
+            this.lefttext.TabIndex = 0;
+            // 
+            // righttext
+            // 
+            this.righttext.Location = new System.Drawing.Point(285, 90);
+            this.righttext.Name = "righttext";
+            this.righttext.Size = new System.Drawing.Size(120, 20);
+            this.righttext.TabIndex = 0;
+            // 
+            // leftcom
+            // 
+            this.leftcom.FormattingEnabled = true;
+            this.leftcom.Items.AddRange(new object[] {
+            "Milimeter (mm)",
+            "Centimeter (cm)",
+            "Meter (m)",
+            "Kilometer (km)"});
+            this.leftcom.Location = new System.Drawing.Point(149, 60);
+            this.leftcom.Name = "leftcom";
+            this.leftcom.Size = new System.Drawing.Size(120, 21);
+            this.leftcom.TabIndex = 1;
+            // 
+            // rightcom
+            // 
+            this.rightcom.FormattingEnabled = true;
+            this.rightcom.Items.AddRange(new object[] {
+            "Milimeter (mm)",
+            "Centimeter (cm)",
+            "Meter (m)",
+            "Kilometer (km)"});
+            this.rightcom.Location = new System.Drawing.Point(285, 60);
+            this.rightcom.Name = "rightcom";
+            this.rightcom.Size = new System.Drawing.Size(120, 21);
+            this.rightcom.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 165);
+            this.Controls.Add(this.rightcom);
+            this.Controls.Add(this.leftcom);
+            this.Controls.Add(this.righttext);
+            this.Controls.Add(this.lefttext);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lefttext;
+        private System.Windows.Forms.TextBox righttext;
+        private System.Windows.Forms.ComboBox leftcom;
+        private System.Windows.Forms.ComboBox rightcom;
+    }
+}
+
---
+++ SE-Project-Distance-Converter/SE-Project-Distance-Converter/Main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SE_Project_Distance_Converter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            leftcom.SelectedIndex = 0;
+            rightcom.SelectedIndex = 0;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+           
+            this.BackColor = System.Drawing.Color.Gray;
+
+            ComboBox leftcom = new ComboBox();
+            leftcom.Height = 20;
+            leftcom.Width = 120;
+            leftcom.ForeColor = Color.Black;
+            leftcom.Location = new Point(149, 60);
+            leftcom.Items.Add("Milimeter (mm)");
+            leftcom.Items.Add("Centimeter (cm)");
+            leftcom.Items.Add("Meter (m)");
+            leftcom.Items.Add("Kilometer (km)");
+            this.Controls.Add(leftcom);
+
+
+            ComboBox rightcom = new ComboBox();
+            rightcom.Height = 20;
+            rightcom.Width = 120;
+            rightcom.ForeColor = Color.Black;
+            rightcom.Location = new Point(285, 60);
+            rightcom.Items.Add("Milimeter (mm)");
+            rightcom.Items.Add("Centimeter (cm)");
+            rightcom.Items.Add("Meter (m)");
+            rightcom.Items.Add("Kilometer (km)");
+            this.Controls.Add(rightcom);
+
+
+            TextBox lefttext = new TextBox();
+            lefttext.Height = 20;
+            lefttext.Width = 120;
+            lefttext.ForeColor = Color.Blue;
+            lefttext.Location = new Point(149, 90);
+            lefttext.Name = "lefttext";
+            this.Controls.Add(lefttext);
+
+
+            TextBox righttext = new TextBox();
+            righttext.Height = 20;
+            righttext.Width = 120;
+            righttext.ForeColor = Color.Blue;
+            righttext.Location = new Point(285, 90);
+            this.Controls.Add(righttext);
+
+
+            Button Btn = new Button();
+            Btn.Text = " -  CONVERT  -  ";
+            Btn.Height = 30;
+            Btn.Width = 420;
+            Btn.Name = "Btn";
+            Btn.FlatStyle = FlatStyle.Flat;
+            Btn.ForeColor = Color.Wheat;
+            Btn.Location = new Point(0, 135);
+            Btn.FlatAppearance.BorderSize = 0;
+            this.Controls.Add(Btn);
+
+
+            Label ext = new Label();
+            ext.Text = "X";
+            ext.Height = 30;
+            ext.Width = 30;
+            ext.FlatStyle = FlatStyle.Flat;
+            ext.TextAlign = ContentAlignment.MiddleCenter;
+            ext.BackColor = Color.Wheat;
+            ext.ForeColor = Color.Black;
+            ext.Location = new Point(380, 0);
+            this.Controls.Add(ext);
+
+
+
+            Label title1 = new Label();
+            title1.Text = "-  DISTANCE CONVERTER  -";
+            title1.Height = 30;
+            title1.Width = 420;
+            title1.TextAlign = ContentAlignment.MiddleCenter;
+            title1.BackColor = Color.Wheat;
+            title1.ForeColor = Color.Black;
+            title1.Location = new Point(0, 0);
+            this.Controls.Add(title1);
+
+
+            Label dis = new Label();
+            dis.Text = "    ||  DISTANCES  || ";
+            dis.Height = 18;
+            dis.Width = 110;
+            dis.TextAlign = ContentAlignment.MiddleLeft;
+            dis.BackColor = Color.White;
+            dis.ForeColor = Color.Black;
+            dis.Location = new Point(20, 61);
+            this.Controls.Add(dis);
+
+
+            Label val = new Label();
+            val.Text = "    ||     VALUES     || ";
+            val.Height = 18;
+            val.Width = 110;
+            val.TextAlign = ContentAlignment.MiddleLeft;
+            val.BackColor = Color.White;
+            val.ForeColor = Color.Black;
+            val.Location = new Point(20, 91);
+            this.Controls.Add(val);
+
+        }
+    }
+}
